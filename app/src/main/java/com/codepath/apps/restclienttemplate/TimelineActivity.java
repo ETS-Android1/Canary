421e595e6cfbf8ef0f8b759a3baeb7ce97c13e6f
Felix Luciano <felixluciano.a@gmail.com>
2022-02-15 19:12:08 -0500

    Added infinite pagination

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -27,12 +27,14 @@ public class TimelineActivity extends AppCompatActivity {
     List<Tweet> tweets;
     TweetsAdapter adapter;
     SwipeRefreshLayout swipeContainer;
+    EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
         client = CanaryApp.getRestClient(this);
+
         swipeContainer = findViewById(R.id.swipeContainer);
 
         swipeContainer.setColorSchemeColors(
@@ -52,30 +54,62 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets = findViewById(R.id.rvTweets);
         tweets = new ArrayList<>();
         adapter = new TweetsAdapter(this,tweets);
-        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rvTweets.setLayoutManager(layoutManager);
         rvTweets.setAdapter(adapter);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                Log.i(TAG,"ONLOADMORE "+page);
+                loadMoreData();
+            }
+        };
+        rvTweets.addOnScrollListener(scrollListener);
+
         populateHomeTimeline();
     }
 
-    private void populateHomeTimeline() {
-        client.getHomeTimeline(new JsonHttpResponseHandler() {
+    private void loadMoreData() {
+        client.getNextPageOfTweets(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG,"Next Page Success");
                 JSONArray jsonArray = json.jsonArray;
                 try {
-                    Log.i(TAG,"Success "+jsonArray.getJSONObject(4).toString());
-                    adapter.clear();
                     adapter.addAll(Tweet.fromJsonArray(jsonArray));
-                    swipeContainer.setRefreshing(false);
                 } catch (JSONException e) {
-                    Log.e(TAG,"JSON Exception",e);
+                    Log.e(TAG,"Next PageJSON Exception",e);
                     e.printStackTrace();
                 }
             }
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.e(TAG, response,throwable);
+                Log.e(TAG, "Next Page request failed"+response,throwable);
+            }
+        },tweets.get(tweets.size()-1).id);
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG,"Success on timeline");
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    adapter.clear();
+                    adapter.addAll(Tweet.fromJsonArray(jsonArray));
+                    swipeContainer.setRefreshing(false);
+                } catch (JSONException e) {
+                    Log.e(TAG,"JSON Exception on timeline",e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "Timeline request failed "+response,throwable);
             }
         });
     }
