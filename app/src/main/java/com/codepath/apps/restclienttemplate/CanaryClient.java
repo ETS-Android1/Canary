31be19c802e414ebfee2b80b56ce6a7faebba38e
Felix Luciano <felixluciano.a@gmail.com>
2022-02-21 21:53:43 -0500

    Tweets now post to twitter

--- app/src/main/java/com/codepath/apps/restclienttemplate/CanaryClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/CanaryClient.java
@@ -61,4 +61,12 @@ public class CanaryClient extends OAuthBaseClient {
 		params.put("max_id",maxId);
 		client.get(apiUrl, params, handler);
 	}
+
+	public void publishTweet(String tweetContent,JsonHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("statuses/update.json");
+		// Can specify query string params directly or through RequestParams.
+		RequestParams params = new RequestParams();
+		params.put("status", tweetContent);
+		client.post(apiUrl, params,"", handler);
+	}
 }
